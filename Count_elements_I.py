13c290af856f6a1fd9dfeff509c42ac9a61ea046
SattiMahesh <99644085+SattiMahesh@users.noreply.github.com>
2022-07-08 21:48:35 +0530

    a,b=map(int,input().split()) l=list(map(int,input().split())) k=list(map(int,input().split())) l=set(l) k=set(k) g=[] for i in l:     if i in k:         g.append(i) print(len(g))

---
+++ Count_elements_I.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+l=list(map(int,input().split()))
+k=list(map(int,input().split()))
+l=set(l)
+k=set(k)
+g=[]
+for i in l:
+    if i in k:
+        g.append(i)
+print(len(g))        
