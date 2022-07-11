58cd45873cf1da8a2ac9b054d16cec2bce48c89f
SattiMahesh <99644085+SattiMahesh@users.noreply.github.com>
2022-07-11 11:01:00 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] for i in l:     c=0     for j in l:         if i==j:             c+=1     if c==1:         a.append(i) if len(a)==0:     print("-1") else:     print(*a)

---
+++ Display_unique_values_in_an_Array.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+a=[]
+for i in l:
+    c=0
+    for j in l:
+        if i==j:
+            c+=1
+    if c==1:
+        a.append(i)
+if len(a)==0:
+    print("-1")
+else:
+    print(*a)
