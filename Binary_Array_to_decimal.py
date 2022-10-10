e9b9bf2183657ac77f5dbdf49d76d72ff5adc830
SattiMahesh <99644085+SattiMahesh@users.noreply.github.com>
2022-10-10 22:37:54 +0530

    t=int(input()) l=list(map(int,input().split())) l1=l[::-1] s=0 for i in range(t):     a=2**i     r=l1[i]*a     s=s+r print(s)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,9 +1,9 @@
-n=int(input())
+t=int(input())
 l=list(map(int,input().split()))
-sum=0
-j=n-1
-for i in range(len(l)):
-    if j>=0:
-        sum=sum+(l[i]*(2**j))
-        j-=1
-print(sum)
+l1=l[::-1]
+s=0
+for i in range(t):
+    a=2**i
+    r=l1[i]*a
+    s=s+r
+print(s)    
