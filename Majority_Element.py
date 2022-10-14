dd8b42050df7249e4e1824a6df196d3df1dcfc2b
SattiMahesh <99644085+SattiMahesh@users.noreply.github.com>
2022-10-14 21:43:21 +0530

    t=int(input()) l=list(map(int,input().split())) max=0 for i in l:     c=0     for j in l:         if i==j:             c+=1     if c>max:         max=c         ele=i print(ele)

---
+++ Majority_Element.py
@@ -0,0 +1,12 @@
+t=int(input())
+l=list(map(int,input().split()))
+max=0
+for i in l:
+    c=0
+    for j in l:
+        if i==j:
+            c+=1
+    if c>max:
+        max=c
+        ele=i
+print(ele)        
