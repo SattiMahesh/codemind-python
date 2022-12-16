a30aa90ce0cab7cc96487d6e3275a0ab08d2da98
SattiMahesh <99644085+SattiMahesh@users.noreply.github.com>
2022-12-16 11:40:34 +0530

    n=int(input()) a=list(map(int,input().split())) k=[] for i in a:     k.append(len(str(i))) k=max(k) m=[] for i in a:     if len(str(i))==k:         m.append(i) l=sorted(m) print(*l)

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -1,9 +1,13 @@
 n=int(input())
-l=list(map(str,input().split()))
-a=[];a2=[]
-for i in l:
-    a.append(len(i))
-for i in range(n):
-    if a[i]==max(a):
-        a2.append(l[i])
-print(*a2)
+a=list(map(int,input().split()))
+k=[]
+for i in a:
+    k.append(len(str(i)))
+k=max(k)
+m=[]
+for i in a:
+    if len(str(i))==k:
+        m.append(i)
+l=sorted(m)
+print(*l)
+        
