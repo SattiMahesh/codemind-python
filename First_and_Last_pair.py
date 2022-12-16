9f32d7660a49aa3dbc6b132b19949ac904d3c7db
SattiMahesh <99644085+SattiMahesh@users.noreply.github.com>
2022-12-16 15:28:38 +0530

    n=int(input()) a=list(map(int,input().split())) k=l=[] for i in range(0,len(a)//2):     k.append(a[i]) for j in range(len(a)//2,len(a)):     l.append(a[j]) l=l[::-1] i=j=0 while i<len(k) or j<len(l):     if i<len(k):         print(k[i],end=' ')         i+=1     if j<len(l):         print(l[j],end=' ')         j+=1 if(n%2!=0):     print(0)

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -1,18 +1,18 @@
 n=int(input())
-l=list(map(int,input().split()))
-a=[]
-b=[]
-c=[]
-k=n//2
-if n%2==1:
-    k+=1
-for i in range(k):
-    a.append(l[i])
-for i in range(n-1,k-1,-1):
-    b.append(l[i])
-if n%2==1:
-    b.append(0)
-for i in range(k):
-    c.append(a[i])
-    c.append(b[i])
-print(*c)
+a=list(map(int,input().split()))
+k=l=[]
+for i in range(0,len(a)//2):
+    k.append(a[i])
+for j in range(len(a)//2,len(a)):
+    l.append(a[j])
+l=l[::-1]
+i=j=0
+while i<len(k) or j<len(l):
+    if i<len(k):
+        print(k[i],end=' ')
+        i+=1
+    if j<len(l):
+        print(l[j],end=' ')
+        j+=1
+if(n%2!=0):
+    print(0)
